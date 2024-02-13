22baa8a0f4e6c8346bcab5fcca81fdd6da58b5e8
Nick Halm <nick.halm@justifi.tech>
2024-02-13 15:48:57 -0600

    Putting everything in the browseronly tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,31 @@
-"use client";
-
 import { Redirect } from "@docusaurus/router";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 
 // import Link from "@docusaurus/Link";
 
-const AiRedirect = () => {
-  return (
-    <BrowserOnly fallback={<div>fdgsdfg</div>}>
-      {() => {
-        window.location.replace("https://docs.justifi.tech");
-        return null;
-      }}
-    </BrowserOnly>
-  );
-};
+// const AiRedirect = () => {
+//   return (
+//     <BrowserOnly>
+//       {() => {
+//         window.location.replace("https://docs.justifi.tech");
+//         return null;
+//       }}
+//     </BrowserOnly>
+//   );
+// };
 
 export default function Home(): JSX.Element {
-  const hostname = window.location.hostname;
+  <BrowserOnly>
+    {() => {
+      const hostname = window.location.hostname;
 
-  if (hostname == "developer.justifi.ai") {
-    return AiRedirect();
-  }
+      if (hostname == "developer.justifi.ai") {
+        window.location.replace("https://docs.justifi.tech");
+        // return AiRedirect();
+      }
+      return null;
+    }}
+  </BrowserOnly>;
 
   return <Redirect to="/gettingStarted" />;
 }
