d4bead8a5e21f22b3a280b06c6d675b9a7872d26
Nick Halm <nick.halm@justifi.tech>
2024-02-13 16:14:32 -0600

    clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,6 @@
 import { Redirect } from "@docusaurus/router";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 
-// import Link from "@docusaurus/Link";
-
-// const AiRedirect = () => {
-//   return (
-//     <BrowserOnly>
-//       {() => {
-//         window.location.replace("https://docs.justifi.tech");
-//         return null;
-//       }}
-//     </BrowserOnly>
-//   );
-// };
-
 export default function Home(): JSX.Element {
   return (
     <BrowserOnly>
