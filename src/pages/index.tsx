d9ff1c1f7c7623df75dfe08a6c661c1d3804f1a3
Nick Halm <nick.halm@justifi.tech>
2024-02-13 14:42:57 -0600

    Moved the redirect to a function so that we can put it in a BrowserOnly tag.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
 import { Redirect } from "@docusaurus/router";
-import Link from "@docusaurus/Link";
+import BrowserOnly from "@docusaurus/BrowserOnly";
+
+// import Link from "@docusaurus/Link";
+
+const AiRedirect = () => {
+  return (
+    <BrowserOnly fallback={<div>fdgsdfg</div>}>
+      {() => {
+        console.log("hereergerg");
+        window.location.replace("https://docs.justifi.tech");
+        return null;
+      }}
+    </BrowserOnly>
+  );
+};
 
 export default function Home(): JSX.Element {
   const hostname = window.location.hostname;
 
   if (hostname == "developer.justifi.ai") {
-    window.location.replace("https://docs.justifi.tech");
-    return;
+    console.log("HERE");
+    return AiRedirect();
+    // window.location.replace("https://docs.justifi.tech");
   }
 
   return <Redirect to="/gettingStarted" />;
