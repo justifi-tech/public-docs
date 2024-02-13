0730fa0d6dbfc374184f3a6841ed858b44413894
Nick Halm <nick.halm@justifi.tech>
2024-02-13 13:56:24 -0600

    I added the redirect to the gettingstarted page.  I also added a url rewrite for the incoming requests from developer.justifi.ai.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,13 @@
-import clsx from "clsx";
-// import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import Heading from "@theme/Heading";
-
-import styles from "./index.module.css";
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
+import { Redirect } from "@docusaurus/router";
+import Link from "@docusaurus/Link";
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+  const hostname = window.location.hostname;
+
+  if (hostname == "developer.justifi.ai") {
+    window.location.replace("https://docs.justifi.tech");
+    return;
+  }
+
+  return <Redirect to="/gettingStarted" />;
 }
